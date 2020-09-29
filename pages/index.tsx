623e788a7ead3562e7f2e0baa8856db8eda735c9
Alperen Karlı <surqurer@gmail.com>
2020-09-29 16:52:29 +0300

    typescript added to project

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Project
+        </h1>
+
+        <p className={styles.description}>
+          Frontend Challenge
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        Alperen Karlı
+      </footer>
+    </div>
+  )
+}
